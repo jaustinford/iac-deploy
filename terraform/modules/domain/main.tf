e5fe9d706fad66766b249a1f06da624e95dc6c64
james austin <j.austin.ford@gmail.com>
2024-08-14 13:36:22 -0600

    organize

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -9,8 +9,8 @@ resource "linode_domain_record" "instances" {
   domain_id   = linode_domain.domain.id
 
   for_each    = {for instance in data.linode_instances.instances.instances: instance.label => instance}
-  name        = each.key
   record_type = "A"
+  name        = each.value["label"]
   target      = each.value["ip_address"]
 }
 
@@ -18,7 +18,7 @@ resource "linode_domain_record" "custom_records" {
   domain_id   = linode_domain.domain.id
 
   for_each    = {for domain_record in var.domain_records: domain_record.name => domain_record}
-  name        = each.key
+  name        = each.value["name"]
   record_type = each.value["record_type"]
   target      = each.value["target"]
 }
