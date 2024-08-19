dbaea784b8ec1b4a338e524dee16a221f8477cfc
james austin <j.austin.ford@gmail.com>
2024-08-19 12:57:01 -0600

    indent for loops

--- terraform/modules/domain/main.tf
+++ terraform/modules/domain/main.tf
@@ -6,19 +6,32 @@ resource "linode_domain" "domain" {
 }
 
 resource "linode_domain_record" "instances" {
-  domain_id   = linode_domain.domain.id
+  domain_id = linode_domain.domain.id
 
-  for_each    = {for instance in data.linode_instances.instances.instances: instance.label => instance}
+  for_each = {
+    for instance in data.linode_instances.instances.instances: instance.label => {
+      label      = instance.label
+      ip_address = instance.ip_address
+    }
+  }
+
   record_type = "A"
-  name        = each.value["label"]
-  target      = each.value["ip_address"]
+  name        = each.value.label
+  target      = each.value.ip_address
 }
 
 resource "linode_domain_record" "custom_records" {
-  domain_id   = linode_domain.domain.id
+  domain_id = linode_domain.domain.id
 
-  for_each    = {for domain_record in var.domain_records: domain_record.name => domain_record}
-  name        = each.value["name"]
-  record_type = each.value["record_type"]
-  target      = each.value["target"]
+  for_each = {
+    for domain_record in var.domain_records: domain_record.name => {
+      name        = domain_record.name
+      record_type = domain_record.record_type
+      target      = domain_record.target
+    }
+  }
+
+  name        = each.value.name
+  record_type = each.value.record_type
+  target      = each.value.target
 }
