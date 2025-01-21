03f27e764d3f0dd8407b657f05c472d237df4d64
james austin <j.austin.ford@gmail.com>
2025-01-21 08:44:18 -0700

    change cider for vpc

--- terraform/environment/main.tf
+++ terraform/environment/main.tf
@@ -2,7 +2,7 @@ module "vpc_elysianskies" {
   source = "../modules/vpc"
 
   vpc_label       = "elysianskies"
-  vpc_subnet_ipv4 = "192.168.600.0/29"
+  vpc_subnet_ipv4 = "192.168.0.0/29"
 }
 
 module "instance_portal" {
