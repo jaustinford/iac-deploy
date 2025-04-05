026fcd1fa9788e3ba2b239aa50f9f5b3402ba582
Austin Ford <j.austin.ford@gmail.com>
2025-04-05 07:52:38 -0600

    conventionalize compose triggers

--- triggers/profile.py
+++ triggers/profile.py
@@ -1,17 +1,29 @@
 """
-Run Compose with script-specific arguments
-and pause the screen.
+Easily build and execute targeted
+compose service profiles.
 """
 
+import os
 import wrappers
 
-PROFILE = input("\n \033[31mprofile\033[0m : ")
+if os.environ.get("COMPOSE_PROFILE"):
+    COMPOSE_PROFILE = os.environ.get("COMPOSE_PROFILE")
+
+else:
+    COMPOSE_PROFILE = input("\n \033[31mcompose profile\033[0m : ")
 
 print(" ")
 
 wrappers.compose_up("builder", "build")
-wrappers.compose_up(PROFILE, "up --detach")
-wrappers.compose_logs(PROFILE)
-wrappers.compose_down(PROFILE)
+wrappers.compose_up(COMPOSE_PROFILE, "up --detach")
+
+try:
+    wrappers.compose_logs(COMPOSE_PROFILE)
+
+except KeyboardInterrupt:
+    wrappers.compose_down(COMPOSE_PROFILE)
+
+else:
+    wrappers.compose_down(COMPOSE_PROFILE)
 
 input("\nPress [enter] to continue... ")
