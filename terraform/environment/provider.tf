5bd22653b5fec77ea9ce2b49987970d2f4d7ecf5
james austin <j.austin.ford@gmail.com>
2025-12-21 14:27:08 -0700

    port over to vault - #24

--- terraform/environment/provider.tf
+++ terraform/environment/provider.tf
@@ -6,9 +6,41 @@ terraform {
       source  = "vancluever/acme"
       version = "2.29.0"
     }
+
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 3.0.0"
+    }
+
+    linode = {
+      source  = "linode/linode"
+      version = ">= 2.0.0"
+    }
   }
 }
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+provider "vault" {
+  address = "https://access.proxy-int.pendragonlab.com"
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id   = jsondecode(file("/approle/iac-deploy.json"))["role_id"]
+      secret_id = jsondecode(file("/approle/iac-deploy.json"))["secret_id"]
+    }
+  }
+}
+
+ephemeral "vault_kv_secret_v2" "linode_api_token" {
+  name  = "external/linode"
+  mount = "lab/kv"
+}
+
+provider "linode" {
+  token  = ephemeral.vault_kv_secret_v2.linode_api_token.data["API_TOKEN"]
 }
