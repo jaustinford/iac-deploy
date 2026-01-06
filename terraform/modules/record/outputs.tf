45197af90bd41251d60e8da1e1865f11ee35bbef
james austin <j.austin.ford@gmail.com>
2026-01-06 12:56:51 -0700

    refactor

--- terraform/modules/record/outputs.tf
+++ terraform/modules/record/outputs.tf
@@ -1,19 +1,19 @@
 output "domain_record_name" {
   description = "Name of DNS record"
-  value       = linode_domain_records.domain_records[*].name
+  value       = linode_domain_record.domain_records[*].name
 }
 
 output "domain_record_target" {
   description = "Target of DNS record"
-  value       = linode_domain_records.domain_records[*].target
+  value       = linode_domain_record.domain_records[*].target
 }
 
 output "domain_record_record_type" {
   description = "Record type of DNS record"
-  value       = linode_domain_records.domain_records[*].record_type
+  value       = linode_domain_record.domain_records[*].record_type
 }
 
 output "domain_record_domain_id" {
   description = "Domain ID of DNS record"
-  value       = linode_domain_records.domain_records[*].domain_id
+  value       = linode_domain_record.domain_records[*].domain_id
 }
